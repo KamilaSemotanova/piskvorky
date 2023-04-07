4332ae38366c4342da29203362d6cf78f37c2f34
Kamila Semotánová <kamila@Kamilas-MacBook-Air.local>
2023-04-07 12:23:14 +0200

    adding hra.js and styling pictograms

---
+++ hra.js
@@ -0,0 +1,12 @@
+let currentPlayer = 'circle'
+
+document.querySelector('button:nth-child(1)')
+document.querySelector('button:nth-child(2)')
+document.querySelector('button:nth-child(3)')
+document.querySelector('button:nth-child(4)')
+document.querySelector('button:nth-child(5)')
+document.querySelector('button:nth-child(6)')
+document.querySelector('button:nth-child(7)')
+document.querySelector('button:nth-child(8)')
+document.querySelector('button:nth-child(9)')
+document.querySelector('button:nth-child(10)')
