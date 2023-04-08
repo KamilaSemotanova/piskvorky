f5a81abdfb1f779dd8fc8eb382c5b7c86ce000cb
Kamila Semotánová <kamila@Kamilas-MacBook-Air.local>
2023-04-08 12:18:13 +0200

    add change of current player in function add class

--- hra.js
+++ hra.js
@@ -1,18 +1,71 @@
 let currentPlayer = 'circle';
 
+const playerCircleElm = `
+<svg width="30" height="30">
+  <circle
+    cx="18"
+    cy="18"
+    r="10"
+    stroke="white"
+    stroke-width="2"
+    fill="transparent"
+  />
+  </svg>`;
+
+const playerCrossElm = `
+<svg width="20" height="20" viewBox="0 -3 5 20" overflow="visible" stroke="white" stroke-width="2">
+  <line 
+    x2="20"
+    y2="20"
+  />
+  <line
+    x1="20"
+    y2="20"
+  />
+  </svg>`;
+
 const addClass = (event) => {
-  event.target.classList.add('board__fieldPlayer--circle');
+  if (currentPlayer === 'circle') {
+    event.target.classList.add('board__fieldPlayer--circle');
+    event.target.disabled = true;
+    currentPlayer = 'cross';
+    document.querySelector(".game__menuPlayerType").innerHTML = playerCrossElm
+  } else {
+    event.target.classList.add('board__fieldPlayer--cross');
+    event.target.disabled = true;
+    currentPlayer = 'circle';
+    document.querySelector(".game__menuLeft").innerHTML = playerCircleElm
+    document.querySelector(".game__menuPlayerType").innerHTML = playerCircleElm
+  }
 };
 
-document.querySelector('#buttonInField1').addEventListener('click', (event) => {
-  event.target.classList.add('board__fieldPlayer--circle');
-});
-document.querySelector('#buttonInField2').addEventListener('click', addClass);
-document.querySelector('#buttonInField3').addEventListener('click', addClass);
-document.querySelector('#buttonInField4').addEventListener('click', addClass);
-document.querySelector('#buttonInField5').addEventListener('click', addClass);
-document.querySelector('#buttonInField6').addEventListener('click', addClass);
-document.querySelector('#buttonInField7').addEventListener('click', addClass);
-document.querySelector('#buttonInField8').addEventListener('click', addClass);
-document.querySelector('#buttonInField9').addEventListener('click', addClass);
-document.querySelector('#buttonInField10').addEventListener('click', addClass);
+document
+  .querySelector('button:nth-child(1)')
+  .addEventListener('click', addClass);
+document
+  .querySelector('button:nth-child(2)')
+  .addEventListener('click', addClass);
+document
+  .querySelector('button:nth-child(3)')
+  .addEventListener('click', addClass);
+document
+  .querySelector('button:nth-child(4)')
+  .addEventListener('click', addClass);
+document
+  .querySelector('button:nth-child(5)')
+  .addEventListener('click', addClass);
+document
+  .querySelector('button:nth-child(6)')
+  .addEventListener('click', addClass);
+document
+  .querySelector('button:nth-child(7)')
+  .addEventListener('click', addClass);
+document
+  .querySelector('button:nth-child(8)')
+  .addEventListener('click', addClass);
+document
+  .querySelector('button:nth-child(9)')
+  .addEventListener('click', addClass);
+document
+  .querySelector('button:nth-child(10)')
+  .addEventListener('click', addClass);
